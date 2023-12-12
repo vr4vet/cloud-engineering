6272a6c8b53d896b437be7963c5014742d981461
unknown <ottevandam@gmail.com>
2023-12-12 16:01:10 +0100

    Typo fix and added step for checking out component table

---
+++ Assets/VR4VET/Components/DataCenter/Scripts/AreaTriggerComponentTable.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Task;
+
+public class AreaTriggerComponentTable : MonoBehaviour
+{
+
+    public Task.TaskHolder taskHolder;
+    // This method is called when another collider enters the trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the entering collider is the player
+        if (other.CompareTag("Player") && taskHolder.GetTask("Perform Maintenance").GetSubtask("Prepare for Maintenance").Compleated())
+        {
+            // Player entered the trigger area
+            taskHolder.GetTask("Perform Maintenance").GetSubtask("Prepare for Maintenance").GetStep("Check Out Component Table").SetCompleated(true);
+        }
+    }
+}
